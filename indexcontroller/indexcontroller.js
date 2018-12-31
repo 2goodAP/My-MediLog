ae0b2fd343d22a33a17cbc6b8264f37c83efcdad
2goodAP <45424904+2goodAP@users.noreply.github.com>
2019-01-01 00:12:36 +0545

    Dashboard layout added

--- indexcontroller/indexcontroller.js
+++ indexcontroller/indexcontroller.js
@@ -9,65 +9,62 @@ exports.medilogIndex = function(req, res) {
 }
 
 module.exports.getPatientLogin = (req,res)=>{
-    var error = (req.cookies.error) ? req.cookies.error : "";
-    res.clearCookie("error");
-    res.render("./patient/login.ejs",{
-        error: error
-    });
+  var error = (req.cookies.error) ? req.cookies.error : "";
+  res.clearCookie("error");
+  res.render("./patient/login.ejs",{
+    error: error
+  });
 };
 
 module.exports.postPatientLogin = (req,res)=>{
-    pool.query("SELECT * FROM patient AS result WHERE email = ? AND password = ?",[req.body.email,md5(req.body.password)],(error,results)=>{
+  pool.query("SELECT * FROM patient AS result WHERE email = ? AND password = ?",[req.body.email,md5(req.body.password)],(error,results)=>{
+    if (error) throw error;
+    if (results.length > 0){
+      var userId = results[0].id;
+      var hash = md5(Date.now());
+      res.cookie("userid",userId,{maxAge: 7*86400000});
+      res.cookie("hash",hash,{maxAge: 7*86400000});
+      pool.query("INSERT INTO sessions (userid, hash) VALUES (?,?)",[userId,hash], (error,results)=>{
         if (error) throw error;
-        if (results.length > 0){
-            var userId = results[0].id;
-            var hash = md5(Date.now());
-
-            res.cookie("userid",userId,{maxAge: 7*86400000});
-            res.cookie("hash",hash,{maxAge: 7*86400000});
-
-            pool.query("INSERT INTO sessions (userid, hash) VALUES (?,?)",[userId,hash], (error,results)=>{
-                if (error) throw error;
-            });
-            res.cookie("name","awan");
-            res.redirect("/patient");
-        }
-        else{
-            res.cookie("error","Login Failed");
-            res.redirect("/patientLogin");
-        }
-    });
+      });
+      res.cookie("name","awan");
+      res.redirect("/patient");
+    } else{
+      res.cookie("error","Login Failed");
+      res.redirect("/patientLogin");
+    }
+  });
 };
 
 exports.getPatientSignup = function(req,res){
-    res.render("./patient/signup.ejs");
+  res.render("./patient/signup.ejs");
 }
 
 exports.patient = function(req,res){
-    res.render("./patient/patient.ejs");
+  res.render("./patient/patient.ejs");
 }
 
 exports.hospitalLogin = function(req,res){
-    res.render("./hospital/login.ejs");
+  res.render("./hospital/login.ejs");
 }
 exports.hospital = function(req,res){
-    res.render("./hospital/hospital.ejs");
+  res.render("./hospital/hospital.ejs");
 }
 
 exports.pharmacy = function(req,res){
-    res.render("./pharmacy/pharmacy.ejs");
-}
-exports.pharmacyLogin = function(req,res){
-    res.render("./pharmacy/login.ejs");
+  res.render("./pharmacy/pharmacy.ejs");
 }
 
+exports.pharmacyLogin = function(req,res){
+  res.render("./pharmacy/login.ejs");
+}
 
 module.exports.patientSignup = (req,res)=>{
-    if(req.body.password===req.body.password2){
-        req.body.password = md5(req.body.password);
-        pool.query("INSERT INTO patient (name, email, password, age, sex, address, phone) VALUES (?,?,?,?,?,?,?)" , [req.body.username,req.body.email,req.body.password,req.body.age,req.body.sex,req.body.address,req.body.phoneNo], (error, results) =>{
-            if(error) throw error;
-            res.send("Sign Up Successful ");
-        });
-    }
+  if(req.body.password===req.body.password2){
+    req.body.password = md5(req.body.password);
+    pool.query("INSERT INTO patient (name, email, password, age, sex, address, phone) VALUES (?,?,?,?,?,?,?)" , [req.body.username,req.body.email,req.body.password,req.body.age,req.body.sex,req.body.address,req.body.phoneNo], (error, results) =>{
+      if(error) throw error;
+      res.send("Sign Up Successful ");
+    });
+  }
 };
