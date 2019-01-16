f1261fb28b07a2c5dd5280adbac70a110e490a08
2goodAP <45424904+2goodAP@users.noreply.github.com>
2019-01-16 14:22:51 +0545

    Changes to dashboard

--- indexcontroller/indexcontroller.js
+++ indexcontroller/indexcontroller.js
@@ -47,8 +47,8 @@ exports.patient = function(req,res){
 }
 
 exports.dashboard = (req, res) => { res.render("./patient/dashboard/index.ejs"); }
-
 exports.dashHistory = (req, res) => { res.render("./patient/dashboard/med_history.ejs"); }
+exports.dashMeds = (req, res) => { res.render("./patient/dashboard/medication.ejs"); }
 
 exports.hospitalLogin = function(req,res){
   res.render("./login/index.ejs");
